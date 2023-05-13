9dbda106a1ab1f1d1fa751b10c713c333396a38b
raunaqt90 <54701972+raunaqt90@users.noreply.github.com>
2023-05-13 20:28:14 +0000

    Lists in Python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+tweets = []
 
 
 @app.route('/')
@@ -8,7 +9,7 @@ def index():
   #return 'Welcome to Raptor Twitter'
   return '''
   <form method="POST" action="postoffice">
-    <lable>What is happening?</label>
+    <h3>What is happening?</h3>
     <br>
     Tweet <input type="text" name="tweet"/>
     <br>
@@ -23,7 +24,20 @@ def index():
 def contact():
   tweet = request.form.get('tweet')
   username = request.form['username']
+  tweets.append(tweet)
   return 'Successfully received tweet: ' + tweet + ' Username: ' + username
+
+
+@app.route('/my-tweets')
+def my_tweets():
+  tweet_html = ""
+  for t in tweets:
+    tweet_html += f'<li>{t}</li>'
+
+  return f'''
+    <h1>All my tweets</h1>
+    <ol>{tweet_html}</ol>
+  '''
 
 
 app.run(host='0.0.0.0', port=81)
